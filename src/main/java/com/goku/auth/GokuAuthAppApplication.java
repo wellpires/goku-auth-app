b9a89060f944c9d9c23d5607d6c144fca2482560
wellpires <wellington.goncalves@live.com>
2020-04-10 19:21:50 -0300

    Commit inicial

---
+++ src/main/java/com/goku/auth/GokuAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.goku.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokuAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuAuthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goku/auth/GokuAuthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goku.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GokuAuthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
